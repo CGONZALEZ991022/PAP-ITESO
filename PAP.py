622f0e2e2e6a7f7fbee6ba2ac29dec8214d64775
CGONZALEZ991022 <114541079+CGONZALEZ991022@users.noreply.github.com>
2022-11-22 20:50:57 -0600

    Update PAP.py

--- PAP.py
+++ PAP.py
@@ -1,14 +1,4 @@
-import pandas as pd  
-import numpy as np
-import matplotlib.pyplot as plt
 import streamlit as st
-import plotly.express as px
-import multiprocessing
-import warnings
-import seaborn as sns
-import datetime as dt
-from datetime import datetime
-from skimage import io
 
 
 st.set_page_config(page_title="Exel Pitss",
